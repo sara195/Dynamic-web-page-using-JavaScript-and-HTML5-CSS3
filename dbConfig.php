ce5392165830e2b6435f9dffa24b398c875a390d
sara195 <62360915+sara195@users.noreply.github.com>
2020-09-05 22:03:16 +0200

    Add files via upload

---
+++ dbConfig.php
@@ -0,0 +1,15 @@
+<?php
+//db details
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'berceau';
+
+//Connect and select the database
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+
+}
+?>
